22bd3db388e61727a2a8056b886d7e17c5212471
4917 Vincent <software@4917.ca>
2019-01-10 18:36:30 -0500

    Removed weird formatting

--- 2019CompetitionBot/src/main/cpp/Robot.cpp
+++ 2019CompetitionBot/src/main/cpp/Robot.cpp
@@ -25,8 +25,7 @@ void Robot::RobotInit() {
  * This function is called every robot packet, no matter the mode. Use
  * this for items like diagnostics that you want ran during disabled,
  * autonomous, teleoperated and test.
- *
- * <p> This runs after the mode specific periodic functions, but before
+ *This runs after the mode specific periodic functions, but before
  * LiveWindow and SmartDashboard integrated updating.
  */
 void Robot::RobotPeriodic() {}
