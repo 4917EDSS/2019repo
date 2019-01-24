b4df9ace6184e1211289c6ab39f7ef6531502329
ethanwarren <ethanraviwarren@gmail.com>
2019-01-24 18:26:56 -0500

    Linked the milky monipulater with the driver controller.

--- 2019CompetitionBot/src/main/cpp/OI.cpp
+++ 2019CompetitionBot/src/main/cpp/OI.cpp
@@ -50,6 +50,8 @@ OI::OI() {
 
   ballFlipperToggleBtn.reset(new frc::JoystickButton(operatorController.get(), FLIP_BALL_INTAKE_TOGGLE_BTN));
   ballFlipperToggleBtn->WhenPressed(new FlipFlipperCmd(FlipFlipperCmd::FlipperDirection::toggle));
+  milkyMonipulaterBtn.reset(new frc::JoystickButton(driverController.get(),MILKY_MONIPULATER_BUTTON));
+  milkyMonipulaterBtn->WhileHeld( new MilkyMonipulaterCmd());
 }
 
 std::shared_ptr<frc::Joystick> OI::getDriverController() {
