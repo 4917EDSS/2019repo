8bbc01d59a16f0fee8a0a6759143c691461b9ad9
4917 Glados <software@4917.ca>
2019-02-02 14:03:07 -0500

    Missed one file

--- 2019CompetitionBot/src/main/include/RobotMap.h
+++ 2019CompetitionBot/src/main/include/RobotMap.h
@@ -22,7 +22,6 @@ constexpr int BALL_INTAKE_WHEELS_MOTOR_CAN_ID = 4;
 constexpr int BALL_INTAKE_FLIP_MOTOR_1_CAN_ID = 8;
 constexpr int BALL_INTAKE_FLIP_MOTOR_2_CAN_ID = 9;
 
-constexpr int ELEVATOR_MOTOR_CAN_ID = 8;
 constexpr int ELEVATOR_MOTOR_1_CAN_ID = 10;
 constexpr int ELEVATOR_MOTOR_2_CAN_ID = 11;
 
