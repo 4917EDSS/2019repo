2e5badcba27dc085f763d0060945c5947bfb9a9a
4917software <46073819+4917software@users.noreply.github.com>
2019-02-14 20:23:42 -0500

    90 case
    Added a case so that the circle x and circley are drawn correctly

--- tools/milkyManipulatorTest.cpp
+++ tools/milkyManipulatorTest.cpp
@@ -1,3 +1,6 @@
+//clang 3.8.0
+
+// Example program
 // Example program
 #include <iostream>
 #include <string>
@@ -11,35 +14,48 @@ using namespace std;
     }
 
     double getCircleCentreY(double circleCentreX, double scoringFaceAngle, double targetX, double targetY){
+        if(tan(fabs(scoringFaceAngle)) > 600000){
+        //vertical scoring face
+            cout<<"yep"<<endl;
+            return ((targetY/2)-((circleCentreX*circleCentreX)/(2*targetY)));
+        }
         return (tan(-scoringFaceAngle) * circleCentreX + (targetY - tan(-scoringFaceAngle) * targetX));
     }
-    double GetRobotTargetAngle(double robotHeading, double cameraAngle, double distance, double scoringFaceAngle){
+    double getRobotTargetAngle(double robotHeading, double cameraAngle, double distance, double scoringFaceAngle, bool comments){
         robotHeading = robotHeading *(M_PI/180);
         cameraAngle = cameraAngle *(M_PI/180);
         scoringFaceAngle = scoringFaceAngle*(M_PI/180);
-        //cout<<"Robot Heading "<<robotHeading<<endl;
-        //cout<<"Camera Angle "<<cameraAngle<<endl;
-        //cout<<"Scoring Face Angle "<<scoringFaceAngle<<endl;
-        //cout<<"cos of pi/2 : "<<cos(M_PI/2)<<endl; 
         double robotX = 0.0;
         double robotY = 0.0;
-        double targetX = (distance*cos((M_PI/2)-robotHeading-cameraAngle));
-        //cout<<"Target X "<<targetX << endl;
-        double targetY = (distance*sin((M_PI/2)-robotHeading-cameraAngle));
-        //cout<<"Target Y "<<targetY<< endl;
+        double targetX = (distance*cos(((M_PI/2)-robotHeading-cameraAngle)));
+        double targetY = (distance*sin(((M_PI/2)-robotHeading-cameraAngle)));
         double circleX = getCircleCentreX(targetX, targetY, robotX, robotY, scoringFaceAngle);
-        //cout<<"Circle X "<<circleX<< endl;
         double circleY = getCircleCentreY(circleX, scoringFaceAngle, targetX, targetY);
-        //cout<<"Circle Y "<<circleY<< endl;
         double r = sqrt(pow(robotX-circleX, 2) + pow(robotY - circleY, 2));
-        //cout<<"R "<<r<< endl;
-        //cout<<"top half"<< (r*r)+(r*r)- sqrt(pow(robotX-circleX, 2) + pow(robotY-(circleY + r), 2))<< endl;
-        //cout<<"everything but inverse cos"<<((r*r)+(r*r) - sqrt(pow(robotX-circleX, 2) + pow(robotY-(circleY + r), 2)))/(2*r*r)<<endl;
-        
-        //cout<<"Final Angle"<<endl;
-        
-        return (acos(((r*r)+(r*r) - sqrt(pow(robotX-circleX, 2) + pow(robotY-(circleY + r), 2)))/(2*r*r)));
+        if(comments){
+        cout<<"Robot Heading "<<robotHeading<<endl;
+        cout<<"Camera Angle "<<cameraAngle<<endl;
+        cout<<"Scoring Face Angle "<<scoringFaceAngle<<endl;
+        cout<<"cos of pi/2 : "<<cos(M_PI/2)<<endl; 
+        cout<<"Target X "<<targetX << endl;
+        cout<<"Target Y "<<targetY<< endl;
+        cout<<"Circle X "<<circleX<< endl;
+        cout<<"Circle Y "<<circleY<< endl;
+        cout<<"R "<<r<< endl;
+        cout<<"top half: "<< (r*r)+(r*r)- sqrt(pow(robotX-circleX, 2) + pow(robotY-(circleY + r), 2))<< endl;
+        cout<<"everything but inverse cos "<<((r*r)+(r*r) - sqrt(pow(robotX-circleX, 2) + pow(robotY-(circleY + r), 2)))/(2*r*r)<<endl;
+        cout<<"Final Angle"<<endl;
+        }
+        return 90+(((acos(((r*r)+(r*r) - sqrt(pow(robotX-circleX, 2) + pow(robotY-(circleY + r), 2)))/(2*r*r))))*(180/M_PI)); //in degrees
+        //return 90+(acos((pow(robotX-targetX, 2) + pow(robotY-targetY, 2) - 2*(r*r))/-(2*r*r)))*(180/M_PI);
+        //return 90+sqrt()
     }
+    
 int main(){
-    cout<<GetRobotTargetAngle(0.0,0.0,1000.0,0.0);
+    cout<<tan(3*M_PI/2)<<endl;
+
+    getRobotTargetAngle(35.0,0.0,1000.0,151.25, true);
+    for(int i = 500; i > 0; i = i-10){
+    cout<<getRobotTargetAngle(120.0,0.0,i,90.0, false)<<endl;
+    }
 }
