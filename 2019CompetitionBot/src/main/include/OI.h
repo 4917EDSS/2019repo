2482d8cdc0686f809d58ba357d930c4a7f3a2f1f
4917 Atlas <software@4917.ca>
2019-02-18 10:42:25 -0500

    setIntakeArmMotor

--- 2019CompetitionBot/src/main/include/OI.h
+++ 2019CompetitionBot/src/main/include/OI.h
@@ -42,13 +42,15 @@ constexpr int HATCH_CONTRACT_BTN = 3;
 constexpr int SET_INTAKE_MOTOR_BTN = 2;
 constexpr int OPERATOR_KILL_ONE_BTN = 11;
 constexpr int OPERATOR_KILL_TWO_BTN = 12;
-constexpr int OPERATOR_ELEVATOR_AXIS = 3;
-constexpr int OPERATOR_MANIPULATOR_AXIS = 1;
+
 constexpr int CLIMB_MODE_BTN = 5;
 constexpr int TEST_BTN = 6;
 constexpr int SET_MANIPULATOR_ENCODER_ZERO_BTN = 8;
 constexpr int RESET_INTAKE_BTN = 7;
 
+constexpr int OPERATOR_MANIPULATOR_AXIS = 1;
+constexpr int OPERATOR_INTAKE_ARM_AXIS = 2;
+constexpr int OPERATOR_ELEVATOR_AXIS = 3;
 
 class OI {
  public:
