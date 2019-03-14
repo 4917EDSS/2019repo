5321b83beeb2a2e6f906cef9bfc073d4855ecefe
4917 Glados <software@4917.ca>
2019-03-14 18:46:30 -0400

    added step in ball intake to make sure manipulator doesn't hit elevator

--- 2019CompetitionBot/src/main/cpp/commands/IntakeBallGrp.cpp
+++ 2019CompetitionBot/src/main/cpp/commands/IntakeBallGrp.cpp
@@ -18,8 +18,9 @@
 IntakeBallGrp::IntakeBallGrp() {
   AddSequential(new FoldIntakeCmd(false));
   AddParallel(new SetIntakeArmAngleCmd(false, INTAKE_CARGO_ANGLE));
-  AddSequential(new SetElevatorToHeightCmd(ELEVATOR_CARGO_FLOOR_PICKUP_HEIGHT_MM));
+  AddSequential(new SetElevatorToHeightCmd(ELEVATOR_MID_SAFE_HEIGHT));
   AddSequential(new SetManipulatorAngleCmd(MANIPULATOR_CARGO_FLOOR_PICKUP_ANGLE));
+  AddSequential(new SetElevatorToHeightCmd(ELEVATOR_CARGO_FLOOR_PICKUP_HEIGHT_MM));
   
   // Enable intake and manipulator wheels, wait for ball detection, disable wheels
   AddSequential(new IntakeBallFromRobotCmd());
