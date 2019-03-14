be6739cee39b63a4f786c0fa12b54d6bbc149493
Eric Gaudet <Eric Gaudet>
2019-03-14 12:26:36 -0400

    Move the power calcs near top of function to reduce spots where calculated.  Make the math sign-friendly.  Break up the math into easier-to-understand lines.

--- 2019CompetitionBot/src/main/cpp/commands/DriveWithJoystickCmd.cpp
+++ 2019CompetitionBot/src/main/cpp/commands/DriveWithJoystickCmd.cpp
@@ -46,7 +46,7 @@ void DriveWithJoystickCmd::Execute() {
 	// In climb mode the intake wheels are used as extra drive wheels
 	if (Robot::inClimbMode) {
 		if (fabs(leftStick) > JOYSTICK_DEADBAND) {
-			Robot::ballIntakeSub.setIntakeWheelPower(leftStick);
+			Robot::ballIntakeSub.setIntakeWheelPower(leftStick);	// TODO:  Should this be exponential instead of linear?
 		}
 		else {
 			Robot::ballIntakeSub.setIntakeWheelPower(0);
@@ -54,6 +54,23 @@ void DriveWithJoystickCmd::Execute() {
 	}
 
 	// Set drivetrain motors based on left and right driver sticks
+	
+	// Calculate the power we would want to apply
+	double drivePower;
+	drivePower = fabs(leftStick);					// Take the sign away so we don't have deal with + vs - right now
+	drivePower -= JOYSTICK_DEADBAND;				// Subtract off the deadband so we start at '0' power once we get past the deadband
+	drivePower = pow(drivePower, DRIVE_POW_EXPONENT); 	// Take the magnitude and raise it to the definied power to get an exponential response to the joysticks
+	drivePower += DRIVE_MINIMUM_OUTPUT_POWER;		// Add the minimum power that will make the machanism move (i.e. overcome static friction)
+	drivePower = std::min(drivePower, MAX_POWER);	// Ensure that power isn't more than max
+	drivePower *= (leftStick < 0) ? -1.0 : 1.0;		// Now that the math is done, re-add the sign (i.e. direction)
+
+	double rotatePower;
+	rotatePower = fabs(rightStick);					// Take the sign away so we don't have deal with + vs - right now
+	rotatePower -= JOYSTICK_DEADBAND;				// Subtract off the deadband so we start at '0' power once we get past the deadband
+	rotatePower = pow(rotatePower, ROTATE_POW_EXPONENT); 	// Take the magnitude and raise it to the definied power to get an exponential response to the joysticks
+	rotatePower += ROTATE_MINIMUM_OUTPUT_POWER;		// Add the minimum power that will make the machanism move (i.e. overcome static friction)
+	rotatePower = std::min(rotatePower, MAX_POWER);	// Ensure that power isn't more than max
+	rotatePower *= (rightStick < 0) ? -1.0 : 1.0;	// Now that the math is done, re-add the sign (i.e. direction)
 
 	if (fabs(leftStick) < JOYSTICK_DEADBAND) {
 		// Left stick is idle.  Make sure that we disable "drive-straight".
@@ -68,22 +85,11 @@ void DriveWithJoystickCmd::Execute() {
 		}
 		else {
 			// Right stick is not idle so rotate the robot
-			double rotatePower = pow(rightStick - JOYSTICK_DEADBAND, ROTATE_POW_EXPONENT) + ROTATE_MINIMUM_OUTPUT_POWER;
-
-			if (rotatePower >= 0) {
-				rotatePower = std::min(rotatePower, MAX_POWER);
-			}
-			else {
-				rotatePower = std::max(rotatePower, -MAX_POWER);
-			}
-
 			Robot::drivetrainSub.drive(rotatePower, -rotatePower);
 		}
 	}
 	else if (fabs(rightStick) < JOYSTICK_DEADBAND) {
 		// Right stick is idle and left stick is not.  We want to drive straight.
-		double drivePower = pow(leftStick - JOYSTICK_DEADBAND, DRIVE_POW_EXPONENT) + DRIVE_MINIMUM_OUTPUT_POWER;
-
 		if (wasDrivingStraight == 0) {
 			// Get ready to drive straight.  Don't start now since AHRS might not have the correct heading angle yet
 			timeSinceDrivingStraight = RobotController::GetFPGATime();
@@ -95,32 +101,17 @@ void DriveWithJoystickCmd::Execute() {
 			wasDrivingStraight = 2;
 		}
 
-		if (drivePower >= 0) {
-			drivePower = std:: min(drivePower, MAX_POWER);
-		}
-		else {
-			drivePower = std::max(drivePower, -MAX_POWER);
-		}
 		Robot::drivetrainSub.driverDriveStraight(-(drivePower));
 	} 
 	else {
 		// Neither joystick is in the deadband so drive and turn.  Make sure that we disable "drive-straight".
-		double drivePower = pow(leftStick - JOYSTICK_DEADBAND, DRIVE_POW_EXPONENT) + DRIVE_MINIMUM_OUTPUT_POWER;
-		double turnPower = pow(rightStick - JOYSTICK_DEADBAND, DRIVE_POW_EXPONENT) + DRIVE_MINIMUM_OUTPUT_POWER;
-
 		if (wasDrivingStraight > 0) {
 			Robot::drivetrainSub.disableBalancerPID();
 			wasDrivingStraight = 0;
 		}
-		if (drivePower >= 0) {
-			drivePower = std::min(drivePower, MAX_POWER);
-		}
-		else {
-			drivePower = std::max(drivePower, -MAX_POWER);
-		}
 
-		double fastSide = std::max(fabs(drivePower), fabs(turnPower));
-    	double slowSide = -drivePower + fabs(turnPower) * drivePower;
+		double fastSide = std::max(fabs(drivePower), fabs(rotatePower));
+    	double slowSide = -drivePower + fabs(rotatePower) * drivePower;
 
 		if (leftStick < 0) { //if leftStick < 0, leftStick is pushed up
 			if (rightStick < 0) { // 
