7029c47663efdd46a25962a34970a7fddd039c4d
4917 Sonny <software@4917.ca>
2019-03-14 20:58:06 -0400

    some tuned values for intaking

--- 2019CompetitionBot/src/main/include/subsystems/BallIntakeSub.h
+++ 2019CompetitionBot/src/main/include/subsystems/BallIntakeSub.h
@@ -16,7 +16,7 @@
 #include "SparkShuffleboardEntrySet.h"
 
 constexpr double INTAKE_NEUTRAL_ANGLE = 0; // Only angle that works
-constexpr double INTAKE_CARGO_ANGLE = 90;
+constexpr double INTAKE_CARGO_ANGLE = 100;
 
 constexpr int INTAKE_ARM_MODE_DISABLED = 0;
 constexpr int INTAKE_ARM_MODE_AUTO = 1;
