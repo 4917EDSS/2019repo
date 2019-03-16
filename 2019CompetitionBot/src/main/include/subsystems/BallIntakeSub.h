4a7aa50ebb5c11721212dd291970b39839b71939
4917 Glados <software@4917.ca>
2019-03-16 09:09:01 -0400

    Blake + Micah changes

--- 2019CompetitionBot/src/main/include/subsystems/BallIntakeSub.h
+++ 2019CompetitionBot/src/main/include/subsystems/BallIntakeSub.h
@@ -16,7 +16,7 @@
 #include "SparkShuffleboardEntrySet.h"
 
 constexpr double INTAKE_NEUTRAL_ANGLE = 0; // Only angle that works
-constexpr double INTAKE_CARGO_ANGLE = 100;
+constexpr double INTAKE_CARGO_ANGLE = 112;
 
 constexpr int INTAKE_ARM_MODE_DISABLED = 0;
 constexpr int INTAKE_ARM_MODE_AUTO = 1;
