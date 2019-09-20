80f1faa888ea0f926ca12849ce11b7b15b5f08ff
4917 Asus <software@4917.ca>
2019-09-20 11:42:55 -0400

    Ben & Mathieu Practice

---
+++ practice/BabyBird/src/main/cpp/subsystems/AreaFiftyOneSub.cpp
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "subsystems/AreaFiftyOneSub.h"
+#include <RobotMap.h>
+
+AreaFiftyOneSub::AreaFiftyOneSub() : Subsystem("AreaFiftyOneSub") {
+  leftMotor1.reset(new rev::CANSparkMax(LEFT_DRIVE_MOTOR_1_CAN_ID, rev::CANSparkMaxLowLevel::MotorType::kBrushless));
+  rightMotor1.reset(new rev::CANSparkMax(RIGHT_DRIVE_MOTOR_1_CAN_ID, rev::CANSparkMaxLowLevel::MotorType::kBrushless));
+}
+
+void AreaFiftyOneSub::InitDefaultCommand() {
+  // Set the default command for a subsystem here.
+  // SetDefaultCommand(new MySpecialCommand());
+  
+}
+void AreaFiftyOneSub::drive(double lPower, double rPower)
+{
+  leftMotor1->Set(lPower);
+  rightMotor1->Set(rPower);
+}
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
---
+++ practice/BabyBird/src/main/include/subsystems/AreaFiftyOneSub.h
@@ -0,0 +1,25 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc/commands/Subsystem.h>
+#include <rev/CANSparkMax.h>
+#include <rev/CANSparkMaxLowLevel.h>
+
+class AreaFiftyOneSub : public frc::Subsystem {
+ private:
+  // It's desirable that everything possible under private except
+  // for methods that implement subsystem capabilities
+  std::shared_ptr <rev::CANSparkMax> leftMotor1;
+  std::shared_ptr <rev::CANSparkMax> rightMotor1;
+
+ public:
+  AreaFiftyOneSub();
+  void InitDefaultCommand() override;
+  void drive(double rPower, double lPower);
+};
